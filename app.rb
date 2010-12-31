dad860605e2a5c5aec9fe96d19ec708079a3f36c
TAKANO Mitsuhiro <takano32@gmail.com>
2010-12-31 19:20:27 +0900

    improve '/' interface

--- app.rb
+++ app.rb
@@ -20,7 +20,7 @@ $users = {'yuiseki'=>'4598697423011019361',
 get '/' do
 	resp = []
 	$users.each_key do |username|
-		resp << "<img src='/#{username}.png?ts=#{Time.now.to_i.to_s}'>"
+		resp << "@#{username}<br/><img src='/#{username}.png?ts=#{Time.now.to_i.to_s}'><hr>"
 	end
 	return resp.join('')
 end
